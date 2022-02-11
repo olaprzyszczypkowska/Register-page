857be9d5938d15d348b546968e2996d164dd8713
Ola <ola.przyszczypkowska@gmail.com>
2022-02-11 23:43:58 +0100

    added inputs styling on click

--- main.js
+++ main.js
@@ -1,12 +1,29 @@
-const inputText = document.querySelector('.form__text');
-const input = document.querySelector('.form__input');
+// const inputText = document.querySelector('.form__text');
+// const input = document.querySelector('.form__input');
 
 
-const textUp = () => {
-    inputText.classList.add('textUp');
-    input.classList.add('inputColor');
-}
+// const textUp = () => {
+//     inputText.classList.add('textUp');
+//     input.classList.add('inputColor');
+// }
 
-input.addEventListener('click',textUp)
+// input.addEventListener('click',textUp)
 
+
+const inputPassword = document.querySelector('.input__password');
+
+const showBtn = document.querySelector ('.btn');
+
+
+showBtn.addEventListener('click', 
+function(){
+    if(inputPassword.matches('[type="password"]')){
+        inputPassword.setAttribute('type','text')
+        showBtn.textContent ='hide';
+    } else{
+        inputPassword.setAttribute('type','password')
+        showBtn.textContent ='show';
+
+    };
+})
 
