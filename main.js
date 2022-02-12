df8da80b2fdced166a62631d7013e17cf2c107cb
Ola <ola.przyszczypkowska@gmail.com>
2022-02-12 01:25:55 +0100

    added validation function

--- main.js
+++ main.js
@@ -1,29 +1,45 @@
-// const inputText = document.querySelector('.form__text');
-// const input = document.querySelector('.form__input');
+const inputPassword = document.querySelector(".input__password");
+const liLetters = document.querySelector('.li__letters')
+const liNumbers = document.querySelector('.li__numbers')
+const liSpecials = document.querySelector('.li__specials')
+
+const showBtn = document.querySelector(".btn");
+
+showBtn.addEventListener("click", function () {
+  if (inputPassword.matches('[type="password"]')) {
+    inputPassword.setAttribute("type", "text");
+    showBtn.textContent = "hide";
+  } else {
+    inputPassword.setAttribute("type", "password");
+    showBtn.textContent = "show";
+  }
+});
+
+const minValue = 8;
+const special = /[!@#$%^&*]/;
+const letters =/[a-z]/i;
+const numbers = /[0-9]/;
 
 
-// const textUp = () => {
-//     inputText.classList.add('textUp');
-//     input.classList.add('inputColor');
-// }
+const passwordValidation = () => {
+if(inputPassword.value.length > minValue && inputPassword.value.match(letters) ){
+    liLetters.classList.add('accept');
+} else{
+    liLetters.classList.remove('accept')
+};
+if (inputPassword.value.match(numbers))  {
+    liNumbers.classList.add('accept')
+    console.log('number');
+} else{
+    liNumbers.classList.remove('accept')
+};
+if(inputPassword.value.match(special)) {
+    liSpecials.classList.add('accept')
+    console.log('special');
+} else {
+    liSpecials.classList.remove('accept')
+}
+}
 
-// input.addEventListener('click',textUp)
 
-
-const inputPassword = document.querySelector('.input__password');
-
-const showBtn = document.querySelector ('.btn');
-
-
-showBtn.addEventListener('click', 
-function(){
-    if(inputPassword.matches('[type="password"]')){
-        inputPassword.setAttribute('type','text')
-        showBtn.textContent ='hide';
-    } else{
-        inputPassword.setAttribute('type','password')
-        showBtn.textContent ='show';
-
-    };
-})
-
+inputPassword.addEventListener('keyup' ,passwordValidation)
